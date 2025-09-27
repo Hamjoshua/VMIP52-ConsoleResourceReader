bb9a01c78c1fd623b0ca4e1961eba24c2054bc0c
Иван Вячеславович Нургазин <ivan.nurgazin@yandex.ru>
2025-09-27 13:22:26 +0700

    Добавление функциональности hashPassword

--- src/main/kotlin/functions/functions.kt
+++ src/main/kotlin/functions/functions.kt
@@ -1,6 +1,11 @@
 package org.example.functions
 
-// TODO вернуть хэш пароля
-fun hashPassword(password: Any, salt: String) : String {
-    return ""
+import java.security.MessageDigest
+import java.util.Base64
+
+fun hashPassword(password: String, salt: String) : String {
+    val digest = MessageDigest.getInstance("SHA-256")
+    val saltedPassword = password + salt
+    val hash = digest.digest(saltedPassword.toByteArray())
+    return Base64.getEncoder().encodeToString(hash)
 }
